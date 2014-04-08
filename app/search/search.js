b9e48311cddf00a444599002b17b97cc777d5cc4
Denis Jacquemin <denis.jacquemin@gmail.com>
2014-04-08 15:18:39 +0200

    regex to match the root state

--- app/search/search.js
+++ app/search/search.js
@@ -5,14 +5,15 @@
 
 angular.module('app.search', [
     'ui.router'
-]).config(function ($stateProvider) {
+]).config(function ($stateProvider, $urlMatcherFactoryProvider) {
 
     //var urlMatcher = $urlMatcherFactory.compile("/");
+    var urlMatcher = $urlMatcherFactoryProvider.compile("/.*");
 
     $stateProvider
 
         .state('root', {
-            url: "/",
+            url: '/{city:.*}',
             data:  {
 
                 lon: 50.8333,
@@ -22,8 +23,8 @@ angular.module('app.search', [
                 'search': {
                     templateUrl: 'search/search.html',
                     resolve: {
-                        location: function (LocationSrvc) {
-                            return LocationSrvc.currentLocationFromIP();
+                        location: function (LocationSrvc, $stateParams) {
+                            return $stateParams.city || LocationSrvc.currentLocationFromIP();
                         }
                     },
                     controller: ['$scope', '$state', 'location',
@@ -82,74 +83,74 @@ angular.module('app.search', [
                     templateUrl: 'map/map.html'
                 }
             }
-        })
-
-
-        .state('root.search', {
-            url: '/:city',
-
-            views: {
-                'search': {
-                    templateUrl: 'search/search.html',
-
-                    controller: ['$scope', '$state', '$location', 'Query', 'querylocation',
-
-                        function ($scope, $state, $stateParams) {
-                            console.log('In root.search.views.search.controller');
-                            $scope.rootSearch = function () {};
-
-                            $scope.doSearch = function (location, lon, lat) {
-                                console.log('doSearch in home ' + location + ', ' + lon + ', ' + lat);
-                                $state.current.data.location = location;
-                                $state.current.data.lon = lon;
-                                $state.current.data.lat = lat;
-                                $state.go('root.search', {city: $state.current.data.location});
-                            };
-                        }]
-                },
-                'properties': {
-                    template: "<p>properties</p>"//'properties/properties.html',
-//                    resolve: {
-//                        showProperties: function () { return true; },
-//                        initProperties: function (PropertySrvc, Query) {
-//                            console.log('initProperties with ' + Query.location);
-//                            // get lon and lat of city
-//                            // then call PropertySrvc.getByGeo function
-//
-//                            return PropertySrvc.loadProperties(Query.location);
-//                        }
-//                    },
-//                    controller: [
-//                        '$scope',
-//                        'Results',
-//                        'initProperties',
-//                        'showProperties',
-//
-//                        function (
-//                            $scope,
-//                            Results,
-//                            initProperties,
-//                            showProperties
-//                        ) {
-//
-//                            $scope.results = Results;
-//                            $scope.results.properties = initProperties;
-//                            $scope.showProperties = showProperties;
-//
-//                        }]
-                },
-                'property': {
-                    templateUrl: 'property/property.html',
-                    controller: function ($scope) {
-
-                        $scope.showProperty = false;
-                    }
-                },
-                'map': {
-                    templateUrl: 'map/map.html'
-                }
-            }
         });
+
+
+//        .state('root.search', {
+//            url: '/:city',
+//
+//            views: {
+//                'search': {
+//                    templateUrl: 'search/search.html',
+//
+//                    controller: ['$scope', '$state', '$location', 'Query', 'querylocation',
+//
+//                        function ($scope, $state, $stateParams) {
+//                            console.log('In root.search.views.search.controller');
+//                            $scope.rootSearch = function () {};
+//
+//                            $scope.doSearch = function (location, lon, lat) {
+//                                console.log('doSearch in home ' + location + ', ' + lon + ', ' + lat);
+//                                $state.current.data.location = location;
+//                                $state.current.data.lon = lon;
+//                                $state.current.data.lat = lat;
+//                                $state.go('root.search', {city: $state.current.data.location});
+//                            };
+//                        }]
+//                },
+//                'properties': {
+//                    template: "<p>properties</p>"//'properties/properties.html',
+////                    resolve: {
+////                        showProperties: function () { return true; },
+////                        initProperties: function (PropertySrvc, Query) {
+////                            console.log('initProperties with ' + Query.location);
+////                            // get lon and lat of city
+////                            // then call PropertySrvc.getByGeo function
+////
+////                            return PropertySrvc.loadProperties(Query.location);
+////                        }
+////                    },
+////                    controller: [
+////                        '$scope',
+////                        'Results',
+////                        'initProperties',
+////                        'showProperties',
+////
+////                        function (
+////                            $scope,
+////                            Results,
+////                            initProperties,
+////                            showProperties
+////                        ) {
+////
+////                            $scope.results = Results;
+////                            $scope.results.properties = initProperties;
+////                            $scope.showProperties = showProperties;
+////
+////                        }]
+//                },
+//                'property': {
+//                    templateUrl: 'property/property.html',
+//                    controller: function ($scope) {
+//
+//                        $scope.showProperty = false;
+//                    }
+//                },
+//                'map': {
+//                    templateUrl: 'map/map.html'
+//                }
+//            }
+//        });
 
 });
 
